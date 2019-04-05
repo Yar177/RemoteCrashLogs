7188e39099ee20ef305d008751a4c64cfab30e95
Yar <yarsher.dev@gmail.com>
2019-04-05 10:48:23 -0400

    Initial commit

---
+++ app/src/main/java/com/yarsher/at/remotecrashlogs/DefaultExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.yarsher.at.remotecrashlogs;
+
+public class DefaultExceptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+
+    }
+}
